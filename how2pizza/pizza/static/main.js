cbe2ecd310bcac198e26b8ebda26aa9c3ee1dd16
Ian Adam Naval <ianonavy@gmail.com>
2015-05-04 04:15:27 -0400

    Improve order page mockup
    Add checkboxes for picking existing pizza
    Add 'add your own' with 'add another' link that works with tab focus

--- how2pizza/pizza/static/main.js
+++ how2pizza/pizza/static/main.js
@@ -1,144 +1,21 @@
 'use strict';
 
-var PEOPLE_PER_PIZZA = 3;
-
-// Converts textarea input to list of lists of ingredients
-function getRecords() {
-	var records = [];
-	var input = document.getElementById("records").value;
-	var lines = input.split("\n");
-	for (var i = 0, len = lines.length; i < len; i++) {
-		var line = lines[i];
-		var toppings = line.split(",");
-		toppings = toppings.map(function (elem) { return elem.trim(); });
-		records.push(toppings);
+function addNewPizza() {
+	var typesList = document.getElementById('types-list');
+	if (typesList.lastElementChild.value == '') {
+		typesList.lastElementChild.focus();
+	} else {
+		var newType = document.createElement('input');
+		newType.type = 'text';
+		newType.className = 'pizza-type';
+		newType.name = 'types[]';
+		typesList.appendChild(newType);
+		newType.focus();
 	}
-	return records;
-}
-
-function getToppings() {
-	// todo refactor to combine with getRecords
-	var toppings = [];
-	var input = document.getElementById("records").value;
-	var lines = input.split("\n");
-	for (var i = 0, len = lines.length; i < len; i++) {
-		var line = lines[i];
-		var currentToppings = line.split(",");
-		currentToppings = currentToppings.map(function (elem) {
-			return elem.trim();
-		});
-		currentToppings = currentToppings.filter(function (elem) {
-			return toppings.indexOf(elem) === -1;
-		});
-		toppings = toppings.concat(currentToppings);
-	}
-	return toppings;
-}
-
-function tallyVotes(records, toppings) {
-	var votes = {};
-	for (var i = 0, len = records.length; i < len; i++) {
-		var record = records[i];
-		for (var j = 0, len2 = record.length; j < len2; j++) {
-			var topping = record[j];
-			if (isNaN(votes[topping])) {
-				votes[topping] = 0;
-			}
-			votes[topping]++;
-		}
-	}
-	return votes;
-}
-
-function countVotes(records) {
-	var count = 0;
-	for (var i = 0, len = records.length; i < len; i++) {
-		var record = records[i];
-		count += record.length;
-	}
-	return count;
-}
-
-function getPeoplePerPizza() {
-	return parseInt(document.getElementById("people-per-pizza").value) || PEOPLE_PER_PIZZA;
-}
-
-function getMaxPizzas(records) {
-	return records.length / getPeoplePerPizza();
-}
-
-function getScores(records, toppings) {
-	var pizzaScore = {};
-	var numberOfVotes = tallyVotes(records, toppings);
-	var totalVotes = countVotes(records);
-	var maxPizzas = getMaxPizzas(records);
-	for (var i = 0, len = toppings.length; i < len; i++) {
-		var topping = toppings[i];
-		pizzaScore[topping] = numberOfVotes[topping] / totalVotes * maxPizzas;
-	}
-	return pizzaScore;
-}
-
-function getMaxTopping(pizzaScores) {
-	// surely there's a better way
-	var maxTopping = null;
-	var maxToppingScore = -1;
-	for (var topping in pizzaScores) {
-		var score = pizzaScores[topping];
-		if (score > maxToppingScore) {
-			maxTopping = topping;
-			maxToppingScore = score;
-		}
-	}
-	return maxTopping;
-}
-
-function calculatePizzas(records, toppings) {
-	var pizzaScores = getScores(records, toppings);
-	var numPizzas = {};
-	var maxPizzas = getMaxPizzas(records);
-	console.log(pizzaScores);
-	for (var i = 0; i < maxPizzas; i++) {
-		var topping = getMaxTopping(pizzaScores);
-		if (!numPizzas[topping]) {
-			numPizzas[topping] = 0;
-		}
-		console.log(numPizzas[topping]);
-		numPizzas[topping]++;
-		pizzaScores[topping]--;
-	}
-	return numPizzas;
-}
-
-function updatePizzaList() {
-	var records = getRecords();
-	var toppings = getToppings();
-	var pizzasToOrder = calculatePizzas(records, toppings);
-	var outputList = document.getElementById("pizza-list");
-	while (outputList.hasChildNodes()) {
-		outputList.removeChild(outputList.firstChild);
-	}
-	for (var topping in pizzasToOrder) {
-		var li = document.createElement('li');
-		var count = pizzasToOrder[topping];
-		li.textContent = count + " " + topping + " pizza" + (count == 1 ? "" : "s");
-		outputList.appendChild(li);
-	}
-}
-
-function updateShouldFeedCount() {
-	var records = getRecords();
-	document.getElementById("num-people").textContent = records.length;
-	document.getElementById("people").textContent = records.length == 1 ? "person" : "people";
-}
-
-function updateUI() {
-	updatePizzaList();
-	updateShouldFeedCount();
 }
 
 function main() {
-	document.addEventListener('keyup', updateUI);
+	document.getElementById("add-pizza").addEventListener('focus', addNewPizza);
 }
 
 main();
